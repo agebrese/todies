876842c9d1d4670510976b9fabc7564d2472e5e6
Abraham <your@email.here>
2023-07-24 14:19:54 -0700

    Feature 3 complete

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+
+USER_MODEL = settings.AUTH_USER_MODEL
+
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    owner = models.ForeignKey(
+        USER_MODEL,
+        related_name="projects",
+        on_delete=models.CASCADE,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
